36a2379c32f3bd5b53d499f6a7b60b7eb8064cd4
gt6989b <gt6989b@gmail.com>
2016-09-07 15:12:22 -0400

    new chgs

--- Java/Euler/src/EulerProb3.java
+++ Java/Euler/src/EulerProb3.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
 class EulerProb3 extends Functor
 {
     /**
@@ -8,9 +12,34 @@ class EulerProb3 extends Functor
 
     EulerProb3(long max) {this.max = max;}
 
+    static ArrayList<Long> sieve(int max)
+    {
+        int     numPrimes = 1 + (int) Math.floor(max/Math.log(max));
+        long [] numbers = LongStream.iterate(2, n -> n+1).limit(max).toArray();
+
+        ArrayList<Long> primes = new ArrayList<>(numPrimes);
+        for (int i = 0; i < numbers.length/2; ++i)
+        {
+            if (numbers[i] > 0)
+            {
+                primes.add(numbers[i]);
+                for (int j = 2 * i; j < numbers.length / 2; ++j)
+                    numbers[j] = 0L;
+            }
+        }
+
+        return primes;
+    }
+
     public String run()
     {
-        long sum = this.max;
-        return String.valueOf(sum);
+        ArrayList<Long> primes = sieve((int) Math.floor(Math.sqrt(this.max)));
+        int primeIndex = primes.size() - 1;
+        for (; primeIndex >= 0; --i)
+            if (this.max % primes.get(i) == 0)
+                break;
+
+        Long answer= (primeIndex >= 0) ? primes.get(primes.size()-1) : this.max;
+        return answer.toString();
     }
 }
