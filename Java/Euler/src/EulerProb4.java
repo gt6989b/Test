01a4629cfb3145ec749a29290d7d3c89873eb197
Dmitry Kreslavskiy <gt6989b@gmail.com>
2016-09-09 10:03:45 -0400

    finishing prob4

--- Java/Euler/src/EulerProb4.java
+++ Java/Euler/src/EulerProb4.java
@@ -5,16 +5,48 @@ import java.util.stream.LongStream;
 class EulerProb4 extends Functor
 {
     /**
-     * The prime factors of 13195 are 5, 7, 13 and 29.
-     * What is the largest prime factor of the number 600851475143?
+     * A palindromic number reads the same both ways. The largest palindrome
+     * made from the product of two 2-digit numbers is 9009 = 91 × 99. Find
+     * the largest palindrome made from the product of two 3-digit numbers.
      */
     private long max;
 
     EulerProb4(long max) {this.max = max;}
 
+    /**
+     * Check string for being a palindrome.
+     * - http://componentsprogramming.com/palindromes/
+     * - http://stackoverflow.com/questions/4138827/check-string-for-palindrome
+     *
+     * @param str String to check for palindrome property
+     * @return True iff it is a palindrome
+     */
+    static boolean isPalindrome(String str)
+    {
+        int n = str.length();
+        for (int i = 0; i < n/2; ++i) {
+            if (str.charAt(i) != str.charAt(n-i-1)) return false;
+        }
+        return true;
+    }
     public String run()
     {
-        int answer = -1;
-        return String.valueOf(answer);
+        int answer = -1, low = -1, high = -1;
+        for (int i = 999; i > 99; --i)
+        {
+            for (int j = i - 1; j > 99; --j)
+            {
+                int product = i * j;
+                if (isPalindrome(String.valueOf(product)) && product > answer) {
+                    answer = product;
+                    low = j;
+                    high = i;
+                }
+            }
+        }
+        return (answer >= 0)
+                ? String.valueOf(answer) + " = " + String.valueOf(low)
+                                         + " * " + String.valueOf(high)
+                : "No palindrome in 3-digit product exists";
     }
 }
