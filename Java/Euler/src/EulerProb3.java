c62daeaebb1e31531e26c46d2482d790ec7d3627
Dmitry Kreslavskiy <gt6989b@gmail.com>
2016-09-09 09:36:12 -0400

    finish problem 3

--- Java/Euler/src/EulerProb3.java
+++ Java/Euler/src/EulerProb3.java
@@ -14,18 +14,27 @@ class EulerProb3 extends Functor
 
     static ArrayList<Long> sieve(int max)
     {
-        int     numPrimes = 1 + (int) Math.floor(max/Math.log(max));
-        long [] numbers = LongStream.iterate(2, n -> n+1).limit(max).toArray();
+        int     numPrimes = 1 + (int) Math.floor(max/Math.log(max)),
+                numIters  = max - 1;
+        long [] numbers
+                   = LongStream.iterate(2, n -> n+1).limit(numIters).toArray();
 
         ArrayList<Long> primes = new ArrayList<>(numPrimes);
-        for (int i = 0; i < numbers.length/2; ++i)
+        int i;
+        for (i = 0; i < numbers.length/2; ++i)
         {
-            if (numbers[i] > 0)
+            long prime = numbers[i];
+            if (prime > 0)
             {
-                primes.add(numbers[i]);
-                for (int j = 2 * i; j < numbers.length / 2; ++j)
+                primes.add(prime);
+                for (int j = i + (int)prime; j < numbers.length; j += prime)
                     numbers[j] = 0L;
             }
+        }
+        for (; i < numbers.length; ++i)
+        {
+            if (numbers[i] > 0)
+                primes.add(numbers[i]);
         }
 
         return primes;
@@ -35,11 +44,11 @@ class EulerProb3 extends Functor
     {
         ArrayList<Long> primes = sieve((int) Math.floor(Math.sqrt(this.max)));
         int primeIndex = primes.size() - 1;
-        for (; primeIndex >= 0; --i)
-            if (this.max % primes.get(i) == 0)
+        for (; primeIndex >= 0; --primeIndex)
+            if (this.max % primes.get(primeIndex) == 0)
                 break;
 
-        Long answer= (primeIndex >= 0) ? primes.get(primes.size()-1) : this.max;
+        Long answer= (primeIndex >= 0) ? primes.get(primeIndex) : this.max;
         return answer.toString();
     }
 }
